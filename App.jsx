79315ff67087f961bcc582d65b50be6ec28f8b7e
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-05-30 18:02:58 +0530

    More to learn

--- App.jsx
+++ App.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
 	render() {
+			var i = 1;
+
 		return (
 			<div>
 				Hello World!!
+			
+				{i == 1 ? 'True' : 'False'}
 			</div>
 		);
 	}
