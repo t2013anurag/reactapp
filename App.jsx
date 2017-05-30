237810c117b1b5ddfd22f8229865b47aed09279d
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-05-30 18:06:10 +0530

    Styling vars

--- App.jsx
+++ App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 class App extends React.Component {
 	render() {
 			var i = 1;
+			var myStyle = {
+				fontSize : 18,
+				color : 'red'
+			}
 
 		return (
-			<div>
+			<div style = {myStyle}>
 				Hello World!!
 			
 				{i == 1 ? 'True' : 'False'}
+
 			</div>
 		);
 	}
