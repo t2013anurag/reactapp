0cc99e17f9ae1233d7145deccfcb14570e045c72
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-05-31 18:10:15 +0530

    Multiple stateless components

--- App.jsx
+++ App.jsx
@@ -14,8 +14,35 @@ class App extends React.Component {
 			
 				{i == 1 ? 'True' : 'False'}
 
+				<Header/>
+				<Content/>
+
 			</div>
 		);
 	}
 }
+
+// other compnents
+
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				Hello Doe, John
+			</div>
+		);
+	}
+}
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div>
+				Welcome Doe, John. This is React.
+			</div>
+		);
+	}
+}
+
+
 export default App;
