9d5729a4c95e399b03853acba29f267c5fc74f58
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-05-31 18:19:25 +0530

    Stateful components

--- App.jsx
+++ App.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 
 class App extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			data : [
+				{
+					"id" : 1,
+					"name" : "John",
+					"age" : 20
+				},
+				{
+					"id" : 2,
+					"name" : "Johnny",
+					"age" : 40
+				},
+				{
+					"id" : 3,
+					"name" : "Johnathan",
+					"age" : 40
+				},
+			]
+		}
+	}
+
 	render() {
 			var i = 1;
 			var myStyle = {
@@ -17,12 +42,18 @@ class App extends React.Component {
 				<Header/>
 				<Content/>
 
+				<table>
+					<tbody>
+						{this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+					</tbody>
+				</table>
+
 			</div>
 		);
 	}
 }
 
-// other compnents
+// other compnents (Stateless)
 
 class Header extends React.Component {
 	render() {
@@ -40,6 +71,20 @@ class Content extends React.Component {
 			<div>
 				Welcome Doe, John. This is React.
 			</div>
+		);
+	}
+}
+
+// Stateful components 
+
+class TableRow extends React.Component {
+	render() {
+		return (
+			<tr>
+				<td>{this.props.data.id}</td>
+				<td>{this.props.data.name}</td>
+				<td>{this.props.data.age}</td>
+			</tr>
 		);
 	}
 }
